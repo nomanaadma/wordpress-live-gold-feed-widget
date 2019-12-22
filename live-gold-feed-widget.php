b4f025a8ee89fa72f9e5a3b5193c05285f3515ce
Noman Shoukat <nomanaadma@gmail.com>
2019-12-22 22:21:55 +0500

    add plugin to the wordpress plugin activation list

---
+++ live-gold-feed-widget.php
@@ -0,0 +1,10 @@
+<?php 
+
+/*
+*Plugin Name: Live Gold Feed 
+*Description: Live gold Feed widget. Once activated go to Widgets and drag the Live gold Feed widget in the widget area or use shortcode [live-Gold-Feed].
+*Version: 1
+*Author: Noman Shoukat
+*Author URI: https://github.com/nomanaadma
+*/
+
